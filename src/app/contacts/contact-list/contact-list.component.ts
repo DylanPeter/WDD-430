1da66894145f4b6f5b19a5eebc3fb9218c9e2035
Dylan Petersen <petersendylan2000@gmail.com>
2025-05-10 15:41:36 -0600

    fixed the images

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
 import { Contact } from '../contact.model';
-import { NgFor } from '@angular/common'; 
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'cms-contact-list',
   standalone: true,
-  imports: [NgFor],  
+  imports: [NgFor],
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent {
   contacts: Contact[] = [
-    new Contact("1", "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "assets/images/jacksonk.jpg", null),
-    new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "assets/images/barzeer.jpg", null)
+    new Contact(
+      '1',
+      'R. Kent Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      'images/jacksonk.jpg',
+      null
+    ),
+    new Contact(
+      '2',
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      'images/barzeer.jpg',
+      null
+    ),
   ];
 }
